34b9fdc07aa6ae8d5d9c1efbd42a36c8b8dd0bc4
Swaroop122 <kori.swaroop@gmail.com>
2020-07-29 21:12:33 +0530

    Initial commit

---
+++ ValuesController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public int Get(int id1,int id2)
+        {
+            return (id1+id2);
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
+//Global class
+pattern:api/values/{id1}/{id2}
